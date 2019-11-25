c4dc89bd6f8d2028e2a20f5d227c2a06082c43c5
Ralf Kretzschmar <ralf.kretzschmar@googlemail.com>
2019-11-25 17:05:16 +0100

    Move db for testing to postgres

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite://'
+        'postgresql://postgres:@localhost:5432/postgres'
 
 
 class ProductionConfig(Config):
